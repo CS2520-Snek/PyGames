31fe0ee6f6a89fa1338b57f4520f3db482389c9a
jefni03 <nijeffrey2003@gmail.com>
2023-05-14 21:23:17 -0700

    centered

--- menu.py
+++ menu.py
@@ -2,8 +2,6 @@ import sys
 import random
 import pygame
 
-
-
 pygame.init()
 clk = pygame.time.Clock()
 
@@ -16,10 +14,9 @@ pygame.display.set_caption("retro")
 bg = pygame.Color('black')
 white = (255, 255, 255)
 
-font = pygame.font.SysFont('arial', 100)
+font = pygame.font.SysFont('arial', 50)
 
 while True:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -34,9 +31,16 @@ while True:
             if event.key == pygame.K_3:
                 with open("minesweeper.py", "r") as rnf:
                     exec(rnf.read())
+    
     text = font.render(f'RETRO', False, white)
     text2 = font.render(f'1)SNAKE\n2)PONG\n3)MINESWEEPER', False, white)
+    
+    text_rect = text.get_rect(center=(w // 2, h // 4))
+    text2_rect = text2.get_rect(center=(w // 2, h // 2))
+    
     screen.fill(bg)
-    screen.blit(text, (300, 10))
-    screen.blit(text2, (300, 200))
+    screen.blit(text, text_rect)
+    screen.blit(text2, text2_rect)
+    
     pygame.display.update()
+    clk.tick(60)
